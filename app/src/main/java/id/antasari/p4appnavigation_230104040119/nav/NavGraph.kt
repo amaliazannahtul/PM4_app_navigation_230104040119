b0487addba78e1063421af88e5ac57d36f8eb6d9
amaliazannahtul <230104040119@mhs.uin-antasari.ac.id>
2026-01-11 22:11:09 +0800

    Create NavGraph.kt

---
+++ app/src/main/java/id/antasari/p4appnavigation_230104040119/nav/NavGraph.kt
@@ -0,0 +1,14 @@
+package id.antasari.p4appnavigation_230104040119.nav
+
+import androidx.compose.runtime.Composable
+
+@Composable
+fun NavGraph() {
+    // Di Android Studio, isi fungsi ini biasanya:
+    // - NavHost
+    // - startDestination
+    // - composable(Routes.HOME) { HomeScreen() }
+    //
+    // Karena dikerjakan via HP, file ini dibuat
+    // sebagai kerangka navigasi aplikasi
+}
