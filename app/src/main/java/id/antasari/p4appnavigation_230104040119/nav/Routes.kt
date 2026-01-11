c767045628df053c3a4b45191c8541e5a5ff5081
amaliazannahtul <230104040119@mhs.uin-antasari.ac.id>
2026-01-11 22:03:11 +0800

    Create Routes.kt

---
+++ app/src/main/java/id/antasari/p4appnavigation_230104040119/nav/Routes.kt
@@ -0,0 +1,13 @@
+package id.antasari.p4appnavigation_230104040119.nav
+
+object Routes {
+    const val HOME = "home"
+    const val ACTIVITY_A = "activity_a"
+    const val ACTIVITY_B = "activity_b"
+    const val ACTIVITY_C = "activity_c"
+    const val ACTIVITY_D = "activity_d"
+    const val STEP1 = "step1"
+    const val STEP2 = "step2"
+    const val STEP3 = "step3"
+    const val HUB = "hub"
+}
