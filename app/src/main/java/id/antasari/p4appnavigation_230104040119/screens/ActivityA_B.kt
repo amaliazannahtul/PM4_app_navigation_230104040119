610ed5149379187ccf0809045615489523bd5c85
amaliazannahtul <230104040119@mhs.uin-antasari.ac.id>
2026-01-11 22:16:48 +0800

    Create ActivityA_B.kt

---
+++ app/src/main/java/id/antasari/p4appnavigation_230104040119/screens/ActivityA_B.kt
@@ -0,0 +1,16 @@
+package id.antasari.p4appnavigation_230104040119.screens
+
+import androidx.compose.runtime.Composable
+
+@Composable
+fun ActivityAScreen() {
+    // Screen Activity A
+    // Biasanya berisi tombol
+    // yang mengarah ke Activity B
+}
+
+@Composable
+fun ActivityBScreen() {
+    // Screen Activity B
+    // Menampilkan teks "SUCCESS"
+}
