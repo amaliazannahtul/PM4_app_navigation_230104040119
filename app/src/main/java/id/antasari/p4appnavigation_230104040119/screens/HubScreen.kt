5c33af9f3330b6348b4956994885b7383ccd550a
amaliazannahtul <230104040119@mhs.uin-antasari.ac.id>
2026-01-11 22:20:11 +0800

    Create HubScreen.kt

---
+++ app/src/main/java/id/antasari/p4appnavigation_230104040119/screens/HubScreen.kt
@@ -0,0 +1,12 @@
+package id.antasari.p4appnavigation_230104040119.screens
+
+import androidx.compose.runtime.Composable
+
+@Composable
+fun HubScreen() {
+    // Hub Screen
+    // Berisi navigasi seperti Fragment:
+    // - Dashboard
+    // - Messages
+    // - Profile
+}
