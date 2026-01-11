b2fc1bdbc86c668519dddc2c23e66e63a9130c31
amaliazannahtul <230104040119@mhs.uin-antasari.ac.id>
2026-01-11 22:19:10 +0800

    Create StepScreens.kt

---
+++ app/src/main/java/id/antasari/p4appnavigation_230104040119/screens/StepScreens.kt
@@ -0,0 +1,21 @@
+package id.antasari.p4appnavigation_230104040119.screens
+
+import androidx.compose.runtime.Composable
+
+@Composable
+fun StepOneScreen() {
+    // Step 1
+    // Navigasi ke Step 2
+}
+
+@Composable
+fun StepTwoScreen() {
+    // Step 2
+    // Navigasi ke Step 3
+}
+
+@Composable
+fun StepThreeScreen() {
+    // Step 3
+    // Tombol Back ke Step 2
+}
