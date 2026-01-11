27da2d686d6206ef71de75f2a51d1342422204ac
amaliazannahtul <230104040119@mhs.uin-antasari.ac.id>
2026-01-11 22:17:49 +0800

    Create ActivityC_D.kt

---
+++ app/src/main/java/id/antasari/p4appnavigation_230104040119/screens/ActivityC_D.kt
@@ -0,0 +1,19 @@
+package id.antasari.p4appnavigation_230104040119.screens
+
+import androidx.compose.runtime.Composable
+
+@Composable
+fun ActivityCScreen() {
+    // Screen Activity C
+    // Digunakan untuk input:
+    // - Nama
+    // - NIM
+    // Data ini nantinya dikirim ke Activity D
+}
+
+@Composable
+fun ActivityDScreen() {
+    // Screen Activity D
+    // Menampilkan Nama dan NIM
+    // yang dikirim dari Activity C
+}
