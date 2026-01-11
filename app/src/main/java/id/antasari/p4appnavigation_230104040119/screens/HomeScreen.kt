f3c9064d7dd0d0b212ba1300d1fbbf94a15148fa
amaliazannahtul <230104040119@mhs.uin-antasari.ac.id>
2026-01-11 22:15:38 +0800

    Create HomeScreen.kt

---
+++ app/src/main/java/id/antasari/p4appnavigation_230104040119/screens/HomeScreen.kt
@@ -0,0 +1,10 @@
+package id.antasari.p4appnavigation_230104040119.screens
+
+import androidx.compose.runtime.Composable
+
+@Composable
+fun HomeScreen() {
+    // Halaman utama aplikasi
+    // Biasanya berisi tombol navigasi ke:
+    // Activity A, Activity C, Step 1, dan Hub
+}
